c67c02296563cde21929d53b5ffe90ba89097ee8
Shin1122 <big_shin@126.com>
2017-04-02 22:55:12 +0800

    初始化提交
    初始化提交

---
+++ YZNotificationDemo/YZNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZNotificationDemo
+//
+//  Created by Shin on 2017/4/2.
+//  Copyright © 2017年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZNotificationDemo/YZNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZNotificationDemo
+//
+//  Created by Shin on 2017/4/2.
+//  Copyright © 2017年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
